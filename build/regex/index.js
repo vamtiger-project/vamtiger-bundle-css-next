a19c71ed59803ddb1200eb4e2e5cc5bd3f55a02f
James-Alex Matthews <vamtiger@vamtiger.com>
2018-04-12 23:53:03 +0200

    vamtiger-node-typescript-commit

--- build/regex/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sourcemap_1 = require("./sourcemap");
-exports.sourceMap = sourcemap_1.default;
-//# sourceMappingURL=index.js.map
--- build/regex/sourcemap/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: RegExp;
-export default _default;
-export interface Match extends RegExpExecArray {
-    css: string;
-    sourcMapLine: string;
-    sourcMapPrefix: string;
-    sourcFilePath: string;
-    sourcMapSuffix: string;
-}
