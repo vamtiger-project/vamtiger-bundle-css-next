add242c48e079d2910ab3fba5b0bd2ba1fdebfbc
James-Alex Matthews <vamtiger@vamtiger.com>
2018-04-12 23:47:03 +0200

    Saving

---
+++ build/regex/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sourcemap_1 = require("./sourcemap");
+exports.sourceMap = sourcemap_1.default;
+//# sourceMappingURL=index.js.map
